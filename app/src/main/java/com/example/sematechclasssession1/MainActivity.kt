ce90d8cd06b9abaeeebc23d5630cfc59844a43b6
Me_Maleki <me_maleki@isc.co.ir>
2020-10-05 09:38:20 +0330

    ClickEventsActivity is added.

--- app/src/main/java/com/example/sematechclasssession1/MainActivity.kt
+++ app/src/main/java/com/example/sematechclasssession1/MainActivity.kt
@@ -1,51 +1,59 @@
 package com.example.sematechclasssession1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import kotlinx.android.synthetic.main.activity_click_events.*
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    val tag : String = "Main:";
+    val tag: String = "Main:";
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
-        Log.d(tag," On Create!")
+        Log.d(tag, " On Create!")
         setContentView(R.layout.activity_main)
 
+
+
+        saveInfo.setOnClickListener {
+            val countryIntent = Intent(this, ClickEventsActivity::class.java)
+            startActivityForResult(countryIntent, 1000)
+        }
     }
 
 
     override fun onStart() {
         super.onStart()
-        Log.d(tag," On Activity!")
+        Log.d(tag, " On Activity!")
     }
 
     override fun onResume() {
         super.onResume()
-        Log.d(tag," On Resume!")
+        Log.d(tag, " On Resume!")
     }
 
     override fun onPause() {
         super.onPause()
-        Log.d(tag," On Pause!")
+        Log.d(tag, " On Pause!")
     }
 
     override fun onStop() {
         super.onStop()
-        Log.d(tag," On Stop!")
+        Log.d(tag, " On Stop!")
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        Log.d(tag," On Destroy!")
+        Log.d(tag, " On Destroy!")
     }
 
     override fun onRestart() {
         super.onRestart()
-        Log.d(tag," On Restart!")
+        Log.d(tag, " On Restart!")
     }
 
 
-
-    }
+}
